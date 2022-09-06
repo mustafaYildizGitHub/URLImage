b7766a304d9fe31b63b9c6ff052697d9e45f6389
mustafa yildiz <myildiz19@ku.edu.tr>
2022-09-06 21:51:35 +0300

    Download an Image From a URL in Swift

--- URLImage/ViewController.swift
+++ URLImage/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Create URL
+        let url = URL(string: "https://cdn.cocoacasts.com/cc00ceb0c6bff0d536f25454d50223875d5c79f1/above-the-clouds.jpg")!
+
+        DispatchQueue.global().async {
+            // Fetch Image Data
+            if let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    // Create Image and Update Image View
+                    self.myImage.image = UIImage(data: data)
+                }
+            }
+        }
     }
 
 
